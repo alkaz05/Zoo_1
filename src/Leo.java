8d94e35333d0e89ad0bf5f095276c77e55a29147
GlebAP <aristo_look@mail.ru>
2023-10-17 12:31:24 +0300

    Add Leo;

---
+++ src/Leo.java
@@ -0,0 +1,19 @@
+public abstract class Leo extends Animal {
+int weight = 150;
+int paws = 4;
+    String name = "Gerald";
+    void displayInfo(){
+        System.out.println(name);
+    }
+
+    public Leo(String name, double weight, int weight1, int paws, String name1) {
+        super(name, weight);
+        this.weight = weight1;
+        this.paws = paws;
+        this.name = name1;
+    }
+    @Override
+    public abstract void move();
+    @Override
+    public abstract void makeSound();
+}
