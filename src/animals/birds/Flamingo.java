7d1cab7c15ef4dd6597d18dabcce48d44d6c8b88
Konstantin S <skg@genialconsult.ru>
2023-10-18 09:53:44 +0300

    убрал лишний конструктор у фламинго

--- src/animals/birds/Flamingo.java
+++ src/animals/birds/Flamingo.java
@@ -7,13 +7,6 @@ public class Flamingo extends Bird implements ISwimmer {
     int age;
     double height;
 
-
-    public Flamingo(String featherColor, int age, double height) {
-        super(featherColor);
-        this.age = age;
-        this.height = height;
-    }
-
     public Flamingo(String name, double weight, String featherColor, int age, double height) {
         super(name, weight, featherColor);
         this.age = age;
